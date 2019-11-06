bd62c6c77064ac83aed67754ce39376faf6dcda5
toshida <57430379+toshida@users.noreply.github.com>
2019-11-06 16:27:41 +0900

    Change cache files dir

--- setup.py
+++ setup.py
@@ -9,7 +9,9 @@ platform_system = platform.system()
 python_version = "3.7.4"
 plynth_version = "1.3.7"
 
-local_zip_dir = "zip_storage"
+local_zip_dir = "cache_files"
+if not os.path.exists(local_zip_dir):
+    os.mkdir(local_zip_dir)
 
 if platform_system.upper() == "LINUX":
     zip_tmp_file_name = "plynth-"+plynth_version+"_py"+python_version+"_linux_64.zip"
