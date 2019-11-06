b526fba1db3a1653c80317e0dd9621d8c7ed7e3e
toshida <57430379+toshida@users.noreply.github.com>
2019-11-06 16:22:52 +0900

    Download plynth.zip on linux

---
+++ setup.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import platform
+import urllib.request
+
+
+platform_system = platform.system()
+
+python_version = "3.7.4"
+plynth_version = "1.3.7"
+
+local_zip_dir = "zip_storage"
+
+if platform_system.upper() == "LINUX":
+    zip_tmp_file_name = "plynth-"+plynth_version+"_py"+python_version+"_linux_64.zip"
+elif platform_system.upper() == "DARWIN":
+    pass
+
+plynth_zip_url = "https://www.plynth.net/dl/1.3.7/b28ed3f9/" + zip_tmp_file_name
+
+if not os.path.exists(os.path.join(local_zip_dir, zip_tmp_file_name)):
+    urllib.request.urlretrieve(os.path.join(local_zip_dir, zip_tmp_file_name))
+    pass
+
+
