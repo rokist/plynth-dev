f894209d0cede15c7677d98d7bf41ec2cb370389
toshida <57430379+toshida@users.noreply.github.com>
2019-11-13 17:58:37 +0900

    Implements appimage output

--- __utils_src/pdk.py
+++ __utils_src/pdk.py
@@ -529,11 +529,20 @@ def remove_file_or_dir(entry):
 
 
 def release_project(cwdir, project_name, args):
-
     project_name = project_name.strip().strip('/')
     project_dir = os.path.join(cwdir, project_name)
     if not os.path.exists(project_dir):
         pass
+
+    # format linux: 'bare', 'appimage'
+    archive_format = None
+    val = None
+    for arg in args.reverse():
+        if val:
+            if arg == "-f": # format
+                archive_format = val
+        val = arg
+
 
     app_version = "1.0.0"
     package_file = os.path.join(project_dir, "package.json")
@@ -557,12 +566,14 @@ def release_project(cwdir, project_name, args):
 
         break
 
-    print("Copying files...")
-    shutil.copytree(os.path.join(cwdir, PLYNTH_DIR), release_workspace)
 
-
+    if is_linux and (archive_format == "appimage" or archive_format == None):
+        make_appimage(cwdir, release_workspace, project_name, app_version)
     
-    if is_windows or is_linux:
+    elif is_windows or is_linux:
+        print("Copying files...")
+        shutil.copytree(os.path.join(cwdir, PLYNTH_DIR), release_workspace)
+
         def zipdir(path, ziph, dir_inzip):
             for cur, dirs, files in os.walk(path):
                 for file in files:
@@ -600,6 +611,9 @@ def release_project(cwdir, project_name, args):
     
 
     if is_mac:
+        print("Copying files...")
+        shutil.copytree(os.path.join(cwdir, PLYNTH_DIR), release_workspace)
+
         app_name = project_name +".app"
         new_app_dir = os.path.join(release_workspace, app_name)
         os.rename(os.path.join(release_workspace, "Plynth.app"), new_app_dir)
@@ -1433,6 +1447,47 @@ def find_pipenv_venv_location(target_dir, ask_to_install):
 
     return False
 
+
+def make_appimage(cwdir, release_workspace, project_name, app_version):
+    # save as an appimage format
+    os.mkdir(os.path.join(release_workspace))
+    os.mkdir(os.path.join(release_workspace, "usr"))
+    bin_dir = os.path.join(release_workspace, "usr", "bin")
+
+    print("Copying files...")
+    shutil.copytree(os.path.join(cwdir, PLYNTH_DIR), bin_dir)
+    os.rename(os.path.join(cwdir, PLYNTH_DIR, "plynth"), os.path.join(cwdir, PLYNTH_DIR, project_name))
+
+    with open(os.path.join(release_workspace, "AppRun", mode="w") as write_file:
+        text = """#!/bin/sh
+SELF=$(readlink -f "$0")
+HERE=${SELF%/*}
+export PATH="${HERE}/usr/bin/:${HERE}/usr/sbin/:${HERE}/usr/games/:${HERE}/bin/:${HERE}/sbin/${PATH:+:$PATH}"
+export LD_LIBRARY_PATH="${HERE}/usr/lib/:${HERE}/usr/lib/i386-linux-gnu/:${HERE}/usr/lib/x86_64-linux-gnu/:${HERE}/usr/lib32/:${HERE}/usr/lib64/:${HERE}/lib/:${HERE}/lib/i386-linux-gnu/:${HERE}/lib/x86_64-linux-gnu/:${HERE}/lib32/:${HERE}/lib64/${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export PYTHONPATH="${HERE}/usr/share/pyshared/${PYTHONPATH:+:$PYTHONPATH}"
+export XDG_DATA_DIRS="${HERE}/usr/share/${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
+export PERLLIB="${HERE}/usr/share/perl5/:${HERE}/usr/lib/perl5/${PERLLIB:+:$PERLLIB}"
+export GSETTINGS_SCHEMA_DIR="${HERE}/usr/share/glib-2.0/schemas/${GSETTINGS_SCHEMA_DIR:+:$GSETTINGS_SCHEMA_DIR}"
+export QT_PLUGIN_PATH="${HERE}/usr/lib/qt4/plugins/:${HERE}/usr/lib/i386-linux-gnu/qt4/plugins/:${HERE}/usr/lib/x86_64-linux-gnu/qt4/plugins/:${HERE}/usr/lib32/qt4/plugins/:${HERE}/usr/lib64/qt4/plugins/:${HERE}/usr/lib/qt5/plugins/:${HERE}/usr/lib/i386-linux-gnu/qt5/plugins/:${HERE}/usr/lib/x86_64-linux-gnu/qt5/plugins/:${HERE}/usr/lib32/qt5/plugins/:${HERE}/usr/lib64/qt5/plugins/${QT_PLUGIN_PATH:+:$QT_PLUGIN_PATH}"
+EXEC=$(grep -e '^Exec=.*' "${HERE}"/*.desktop | head -n 1 | cut -d "=" -f 2 | cut -d " " -f 1)
+exec "${EXEC}" "$@"
+"""
+        write_file.write(text)
+
+    with open(os.path.join(release_workspace, project_name+".desktop", mode="w") as write_file:
+        text = """[Desktop Entry]
+Name="""+project_name+ """
+Exec="""+project_name+ """
+Icon="""+project_name+ """
+Type=Application
+Categories=Utility;
+"""
+        write_file.write(text)
+
+    try:
+        check_output(['appimagetool', release_workspace], stderr=STDOUT)
+    except CalledProcessError as err:
+        print("zip error: 9807889")
 
 def newproject(cwdir, project_name, rest_args):
     if os.path.exists(project_name) and len(os.listdir(project_name)) > 0:
