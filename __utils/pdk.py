d07cf88a1d549f8b7d697b84bc3e8055ee0206d1
toshida <57430379+toshida@users.noreply.github.com>
2019-11-14 16:52:58 +0900

    Add -l option

--- __utils/pdk.py
+++ __utils/pdk.py
@@ -1511,7 +1511,7 @@ Categories=Utility;
  
     if os.path.exists(local_appimage_path):
         try:
-            check_output([local_appimage_path, "-v", release_workspace], stderr=STDOUT)
+            check_output([local_appimage_path, "-l", "-v", release_workspace], stderr=STDOUT)
         except CalledProcessError as err:
             print("appimagetool error 4891")
     else:
